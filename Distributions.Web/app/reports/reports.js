b65984eb9a78e19c6c1aea2b8563fb606a89654a
Shimi Itach <shimi128@gmail.com>
2015-06-25 21:31:35 +0300

    add scroll in report page

--- Distributions.Web/app/reports/reports.js
+++ Distributions.Web/app/reports/reports.js
@@ -74,8 +74,8 @@
 
         vm.roundFilter = {
             Today: false,
-            StartDate: $filter('date')(new Date(vm.dateFilter.getFullYear(), vm.dateFilter.getMonth() -2, 0).setDate(1), 'MM-dd-yyyy'),
-            EndDate: $filter('date')(new Date(vm.dateFilter.getFullYear(), vm.dateFilter.getMonth() + 2, 1), 'MM-dd-yyyy'),
+            StartDate: $filter('date')(new Date(vm.dateFilter.getFullYear(), vm.dateFilter.getMonth()+1, 0).setDate(1), 'MM-dd-yyyy'),
+            EndDate: $filter('date')(new Date(vm.dateFilter.getFullYear(), vm.dateFilter.getMonth()+1, 1), 'MM-dd-yyyy'),
             ManagerId: cache.get('managerId')
         }
 
